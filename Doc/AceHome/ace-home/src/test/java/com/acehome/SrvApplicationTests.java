dcaca1ec864efd5395a22479be0788ac2ba1750f
Amesika <am3sika@gmail.com>
2021-12-17 23:48:36 +0100

    Mise en place de la structure

---
+++ Doc/AceHome/ace-home/src/test/java/com/acehome/SrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
